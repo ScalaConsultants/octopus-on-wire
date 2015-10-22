0deb909501a5b0791e998146470fa826e423afdb
kubukoz <kubukoz@gmail.com>
2015-10-22 20:11:25 +0200

    Move WindowOperations to another file

--- client/src/main/scala/io/scalac/octopus/client/EventWindowOperations.scala
+++ client/src/main/scala/io/scalac/octopus/client/EventWindowOperations.scala
@@ -48,26 +48,3 @@ object EventWindowOperations extends WindowOperations {
   }
 }
 
-/**
- * Defines basic operations on windows.
- * */
-trait WindowOperations {
-  protected var outsideListener: Div = _
-
-  protected def openWindow(window: Div)(implicit octopusHome: Div) = {
-    octopusHome.appendChild(window)
-    outsideListener = getOutsideListener(window)(octopusHome)
-    octopusHome.appendChild(outsideListener)
-    timers.setTimeout(ClientConfig.WindowOpenDelay)(window.classList.remove("closed"))
-  }
-
-  protected def getOutsideListener(window: Div)(implicit octopusHome: Div): Div = div(`class` := "octopus-outside",
-    onclick := { () => closeWindow(window) }
-  ).render
-
-  protected def closeWindow(window: Div)(implicit octopusHome: Div): Unit = {
-    octopusHome.removeChild(outsideListener)
-    window.classList.add("closed")
-    timers.setTimeout(ClientConfig.WindowLoadTime)(octopusHome.removeChild(window))
-  }
-}
