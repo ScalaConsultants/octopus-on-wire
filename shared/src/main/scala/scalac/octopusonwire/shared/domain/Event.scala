21f60f42c720915ec4fd66450e666b40e8a44cb7
kubukoz <kubukoz@gmail.com>
2015-10-23 13:45:19 +0200

    Add external link and display actual date in event view

--- shared/src/main/scala/scalac/octopusonwire/shared/domain/Event.scala
+++ shared/src/main/scala/scalac/octopusonwire/shared/domain/Event.scala
@@ -4,4 +4,4 @@ package scalac.octopusonwire.shared.domain
  * @author kubukoz
  *         created on 22/10/15.
  */
-case class Event(id: Int, name: String, startDate: Long, endDate: Long, location: String)
+case class Event(id: Int, name: String, startDate: Long, endDate: Long, location: String, url: String)
