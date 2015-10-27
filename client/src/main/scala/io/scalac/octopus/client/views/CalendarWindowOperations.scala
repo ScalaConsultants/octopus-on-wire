c2aa0b7ff9fb61476be7318c1abb59b6f3234768
kubukoz <kubukoz@gmail.com>
2015-10-27 10:46:59 +0100

    Refactor: move CalendarUtils singleton to CalendarView class

--- client/src/main/scala/io/scalac/octopus/client/views/CalendarWindowOperations.scala
+++ client/src/main/scala/io/scalac/octopus/client/views/CalendarWindowOperations.scala
@@ -1,9 +1,11 @@
 package io.scalac.octopus.client.views
 
 import io.scalac.octopus.client.config.ClientConfig
+import io.scalac.octopus.client.tools.DateOps._
 import io.scalac.octopus.client.tools.{DateOps, EventDateOps}
 import org.scalajs.dom.html.Div
-
+import DateOps._
+import EventDateOps._
 import scala.scalajs.js.{Date, timers}
 import scalac.octopusonwire.shared.domain.Event
 import scalatags.JsDom.all._
@@ -25,8 +27,6 @@ object CalendarWindowOperations extends WindowOperations {
         closeWindow(octopusHome)
         None
       case None =>
-        import DateOps._
-        import EventDateOps._
 
         val now = new Date(Date.now())
 
@@ -36,56 +36,9 @@ object CalendarWindowOperations extends WindowOperations {
           div(`class` := "octopus-window-bottom-arrow arrow-left")
         ).render
 
-        object CalendarUtils {
-          def calendarTable(currentMonth: Date): Div = CalendarTable(
-            currentMonth,
-            marker = calendarDay =>
-              (calendarDay isSameMonth currentMonth) && events.exists(_ takesPlaceOn calendarDay),
-            modifier = {
-              case date if (date isSameMonth currentMonth) && events.exists(_ takesPlaceOn date) =>
-                Array(span(date.getDate()),
-                  div(
-                    `class` := "octopus-preview",
-                    events.filter(_ takesPlaceOn date).map { event =>
-                      div(
-                        event.name,
-                        `class` := "octopus-preview-element",
-                        onclick := { () =>
-                          closeWindow(octopusHome)
-                          timers.setTimeout(ClientConfig.WindowOpenDelay)(EventWindowOperations.openEventWindow(event)(octopusHome))
-                        }
-                      )
-                    }
-                  ))
-              case date => Array(date.getDate().toString)
-            }
-          ).render
+        val calendarView  = new CalendarView(window, octopusHome)
 
-          def monthSelector(current: Date): Div = {
-            val yearString = current.getFullYear().toString
-            div(
-              `class` := "octopus-calendar-arrow-wrapper",
-              span(`class` := "octopus-calendar-arrow arrow-left", onclick := { () =>
-                window.replaceChild(tableWithSelector(getPreviousMonth(current)), window.firstChild)
-              }),
-              span(Months(current.getMonth()) + " '" + yearString.substring(yearString.length - 2),
-                `class` := "octopus-calendar-month"
-              ),
-              span(`class` := "octopus-calendar-arrow arrow-right", onclick := { () =>
-                window.replaceChild(tableWithSelector(getNextMonth(current)), window.firstChild)
-              })
-            ).render
-          }
-
-          def tableWithSelector(current: Date) =
-            div(
-              `class` := "octopus-table-wrapper",
-              monthSelector(current),
-              calendarTable(current)
-            ).render
-        }
-
-        window.replaceChild(CalendarUtils.tableWithSelector(now), window.firstChild)
+        window.replaceChild(calendarView(now, events), window.firstChild)
         openWindow(window)(octopusHome)
         Option(window)
     }
@@ -95,5 +48,55 @@ object CalendarWindowOperations extends WindowOperations {
       removeWindow(openedWindow)(octopusHome)
       None
     case None => None
+  }
+}
+
+class CalendarView(window: Div, octopusHome: Div){
+  def apply(current: Date, events: Array[Event]): Div = {
+    val calendarTable: Div = CalendarTable(
+      current,
+      marker = calendarDay =>
+        (calendarDay isSameMonth current) && events.exists(_ takesPlaceOn calendarDay),
+      modifier = {
+        case date if (date isSameMonth current) && events.exists(_ takesPlaceOn date) =>
+          Array(span(date.getDate()),
+            div(
+              `class` := "octopus-preview",
+              events.filter(_ takesPlaceOn date).map { event =>
+                div(
+                  event.name,
+                  `class` := "octopus-preview-element",
+                  onclick := { () =>
+                    CalendarWindowOperations.closeWindow(octopusHome)
+                    timers.setTimeout(ClientConfig.WindowOpenDelay)(EventWindowOperations.openEventWindow(event)(octopusHome))
+                  }
+                )
+              }
+            ))
+        case date => Array(date.getDate().toString)
+      }
+    ).render
+
+    val monthSelector: Div = {
+      val yearString = current.getFullYear().toString
+      div(
+        `class` := "octopus-calendar-arrow-wrapper",
+        span(`class` := "octopus-calendar-arrow arrow-left", onclick := { () =>
+          window.replaceChild(apply(getPreviousMonth(current), events), window.firstChild)
+        }),
+        span(Months(current.getMonth()) + " '" + yearString.substring(yearString.length - 2),
+          `class` := "octopus-calendar-month"
+        ),
+        span(`class` := "octopus-calendar-arrow arrow-right", onclick := { () =>
+          window.replaceChild(apply(getNextMonth(current), events), window.firstChild)
+        })
+      ).render
+    }
+
+    div(
+      `class` := "octopus-table-wrapper",
+      monthSelector,
+      calendarTable
+    ).render
   }
 }
