8c4c3b174f567761ae1be4254a74a8b46b819599
kubukoz <kubukoz@gmail.com>
2015-10-27 11:36:06 +0100

    Add missing feature flags, move CalendarView class to its own file

--- client/src/main/scala/io/scalac/octopus/client/views/CalendarWindowOperations.scala
+++ client/src/main/scala/io/scalac/octopus/client/views/CalendarWindowOperations.scala
@@ -1,12 +1,8 @@
 package io.scalac.octopus.client.views
 
-import io.scalac.octopus.client.config.ClientConfig
-import io.scalac.octopus.client.tools.DateOps._
-import io.scalac.octopus.client.tools.{DateOps, EventDateOps}
 import org.scalajs.dom.html.Div
-import DateOps._
-import EventDateOps._
-import scala.scalajs.js.{Date, timers}
+
+import scala.scalajs.js.Date
 import scalac.octopusonwire.shared.domain.Event
 import scalatags.JsDom.all._
 
@@ -36,7 +32,7 @@ object CalendarWindowOperations extends WindowOperations {
           div(`class` := "octopus-window-bottom-arrow arrow-left")
         ).render
 
-        val calendarView  = new CalendarView(window, octopusHome)
+        val calendarView = new CalendarView(window, octopusHome)
 
         window.replaceChild(calendarView(now, events), window.firstChild)
         openWindow(window)(octopusHome)
@@ -48,55 +44,5 @@ object CalendarWindowOperations extends WindowOperations {
       removeWindow(openedWindow)(octopusHome)
       None
     case None => None
-  }
-}
-
-class CalendarView(window: Div, octopusHome: Div){
-  def apply(current: Date, events: Array[Event]): Div = {
-    val calendarTable: Div = CalendarTable(
-      current,
-      marker = calendarDay =>
-        (calendarDay isSameMonth current) && events.exists(_ takesPlaceOn calendarDay),
-      modifier = {
-        case date if (date isSameMonth current) && events.exists(_ takesPlaceOn date) =>
-          Array(span(date.getDate()),
-            div(
-              `class` := "octopus-preview",
-              events.filter(_ takesPlaceOn date).map { event =>
-                div(
-                  event.name,
-                  `class` := "octopus-preview-element",
-                  onclick := { () =>
-                    CalendarWindowOperations.closeWindow(octopusHome)
-                    timers.setTimeout(ClientConfig.WindowOpenDelay)(EventWindowOperations.openEventWindow(event)(octopusHome))
-                  }
-                )
-              }
-            ))
-        case date => Array(date.getDate().toString)
-      }
-    ).render
-
-    val monthSelector: Div = {
-      val yearString = current.getFullYear().toString
-      div(
-        `class` := "octopus-calendar-arrow-wrapper",
-        span(`class` := "octopus-calendar-arrow arrow-left", onclick := { () =>
-          window.replaceChild(apply(getPreviousMonth(current), events), window.firstChild)
-        }),
-        span(Months(current.getMonth()) + " '" + yearString.substring(yearString.length - 2),
-          `class` := "octopus-calendar-month"
-        ),
-        span(`class` := "octopus-calendar-arrow arrow-right", onclick := { () =>
-          window.replaceChild(apply(getNextMonth(current), events), window.firstChild)
-        })
-      ).render
-    }
-
-    div(
-      `class` := "octopus-table-wrapper",
-      monthSelector,
-      calendarTable
-    ).render
   }
 }
