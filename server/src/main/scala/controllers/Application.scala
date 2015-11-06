dc87bc97cb81e47b20911aff89f27364a3a4da1c
Jakub Kozłowski <kubukoz@gmail.com>
2015-11-06 17:56:32 +0100

    Move GithubApi, UserCache to their own files

--- server/src/main/scala/controllers/Application.scala
+++ server/src/main/scala/controllers/Application.scala
@@ -5,81 +5,37 @@ import java.nio.ByteBuffer
 import boopickle.Default._
 import com.google.common.net.MediaType
 import config.Github._
-import config.{Github, ServerConfig}
-import domain.UserId
-import play.api.Play.current
-import play.api.libs.ws.WS
+import config.{Github, Router, ServerConfig}
 import play.api.mvc._
-import services.{ApiService, EventSource, InMemoryEventSource}
+import services._
 
-import scala.collection.concurrent.TrieMap
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
-import scala.language.postfixOps
+import scala.language.{implicitConversions, postfixOps}
 import scalac.octopusonwire.shared.Api
 import scalac.octopusonwire.shared.domain.EventId
-
-object UserCache {
-
-  //temporary cache
-  val cache = TrieMap[String, UserId]()
-
-  def getOrFetchUserId(token: String): Option[UserId] = cache.get(token).orElse(fetchUserId(token))
-
-  private def fetchUserId(token: String): Option[UserId] = {
-    val result = Await.result(
-      awaitable = WS.url(UserUrl)
-        .withRequestTimeout(ApiRequestTimeout)
-        .withQueryString(AccessToken -> token)
-        .execute("GET"),
-      atMost = Duration.Inf
-    )
-    val uid = (result.json \ "id").toOption.map(id => UserId(id.toString.toInt))
-
-    //update cache
-    uid.foreach(cache(token) = _)
-
-    uid
-  }
-}
-
-object Router extends autowire.Server[ByteBuffer, Pickler, Pickler] {
-  override def read[R: Pickler](p: ByteBuffer) = Unpickle[R].fromBytes(p)
-
-  override def write[R: Pickler](r: R) = Pickle.intoBytes(r)
-}
 
 object Application extends Controller {
   val eventSource: EventSource = InMemoryEventSource
 
-  def index = Action {
-    Ok(views.html.index())
-  }
+  def CorsEnabled(result: Result)(implicit request: Request[Any]): Result =
+    result.withHeaders(
+      ACCESS_CONTROL_ALLOW_ORIGIN -> request.headers(ORIGIN),
+      ACCESS_CONTROL_ALLOW_HEADERS -> CONTENT_TYPE,
+      ACCESS_CONTROL_ALLOW_CREDENTIALS -> "true",
+      CONTENT_TYPE -> MediaType.OCTET_STREAM.`type`)
 
-  def getGithubToken(code: String): Future[Option[String]] = {
-    val result = WS.url(AccessTokenUrl)
-        .withRequestTimeout(ApiRequestTimeout)
-        .withHeaders(ACCEPT -> "application/xml")
-        .withQueryString("client_id" -> ClientId, "client_secret" -> ClientSecret, "code" -> code)
-        .execute("POST")
-
-    result.map(r => Option((r.xml \ AccessToken).text) )
-  }
-
-  private def getUserId(tokenOption: Option[String]): Option[UserId] =
-    tokenOption.flatMap(token => UserCache.getOrFetchUserId(token))
+  def index = Action(Ok(views.html.index()))
 
   def joinEventWithGithub(joinEvent: Long, code: String, sourceUrl: String) = Action.async { request =>
-    getGithubToken(code).map(token => {
-      val userOpt = getUserId(token)
+    GithubApi.getGithubToken(code).map(token => {
+      val userOpt = GithubApi.getUserId(token)
       new ApiService(userOpt)
         .joinEventAndGetJoins(EventId(joinEvent))
-      
+
       Redirect(sourceUrl).withCookies(Cookie(
-        name = AccessToken,
+        name = AccessTokenKey,
         value = token.getOrElse(""),
-        maxAge = token.map(_ => 14 * 3600 * 24),
+        maxAge = token.map(_ => 14 * 3600 * 24).orElse(Some(-1)),
         domain = Some(ServerConfig.Domain),
         secure = false, //we don't have HTTPS yet
         httpOnly = true
@@ -87,18 +43,11 @@ object Application extends Controller {
     })
   }
 
-
-  def CorsEnabled(result: Result)(implicit request: Request[Any]): Result = result.withHeaders(
-    ACCESS_CONTROL_ALLOW_ORIGIN -> request.headers(ORIGIN),
-    ACCESS_CONTROL_ALLOW_HEADERS -> CONTENT_TYPE,
-    ACCESS_CONTROL_ALLOW_CREDENTIALS -> "true",
-    CONTENT_TYPE -> MediaType.OCTET_STREAM.`type`)
-
   def autowireApi(path: String) = Action.async(parse.raw) { implicit request =>
     println(s"Request path: $path")
 
-    val tokenCookie: Option[String] = request.cookies.get(Github.AccessToken).map(_.value)
-    val userOpt = getUserId(tokenCookie)
+    val tokenCookie: Option[String] = request.cookies.get(Github.AccessTokenKey).map(_.value)
+    val userOpt = GithubApi.getUserId(tokenCookie)
 
     val apiService = new ApiService(userOpt)
 
