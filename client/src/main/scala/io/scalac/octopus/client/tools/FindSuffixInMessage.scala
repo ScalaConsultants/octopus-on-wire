9e202ae349a10dab5b609e91138b87b410b03fe7
Jakub Kozłowski <kubukoz@gmail.com>
2015-11-30 22:53:45 +0100

    Start improving event validation

--- client/src/main/scala/io/scalac/octopus/client/tools/FindSuffixInMessage.scala
+++
@@ -1,7 +0,0 @@
-package io.scalac.octopus.client.tools
-
-import scala.language.postfixOps
-
-object FindSuffixInMessage{
-  def apply(message: String, suffixes: Seq[String]): Option[String] = suffixes find (message endsWith)
-}
