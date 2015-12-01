24446d5444352918de8e3202d3814f106a9fe3af
Jakub Kozłowski <kubukoz@gmail.com>
2015-12-01 23:59:16 +0100

    Implement per-field validation

--- client/src/main/scala/io/scalac/octopus/client/views/FormHandler.scala
+++ client/src/main/scala/io/scalac/octopus/client/views/FormHandler.scala
@@ -7,10 +7,10 @@ import io.scalac.octopus.client.config.ClientConfig.{KeyCheckDelay, MoveToCalend
 import io.scalac.octopus.client.tools.DateOps
 import io.scalac.octopus.client.tools.DateOps._
 import io.scalac.octopus.client.tools.TimeUnit._
-import io.scalac.octopus.client.views.FormHandler.getNewTimeField
-import org.scalajs.dom.html.{Button, Div}
-import org.scalajs.dom.raw.{HTMLElement, MouseEvent}
-import org.scalajs.dom.{Event => DomEvent, KeyboardEvent}
+import io.scalac.octopus.client.views.FormHandler.{MalformedDatesMessage, addValidationListeners, getNewTimeField}
+import org.scalajs.dom.KeyboardEvent
+import org.scalajs.dom.html.{Button, Div, Span}
+import org.scalajs.dom.raw.{FocusEvent, HTMLElement, MouseEvent, UIEvent}
 
 import scala.language.postfixOps
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
@@ -22,20 +22,34 @@ import scalac.octopusonwire.shared.tools.LongRangeOps._
 import scalatags.JsDom.all._
 
 class FormHandler(startDay: Date, octopusHome: Div) {
-  var submitted = false
-  val MalformedDatesMessage = "We need to know when the event starts and ends!"
-
-  //todo map contenteditable fields' onkeypressed to buildEventOrGetErrors
-
-  val eventNameField = h1(
+  //text fields
+  val eventNameField = addValidationListeners(h1(
     placeholder := "Event name",
     `class` := "octopus-event-name", contenteditable := "true"
-  ).render
+  ).render, validateForm)
 
-  val startHourField = getNewTimeField("H", 0, 23, maxLength = 2)
-  val startMinuteField = getNewTimeField("MM", 0, 59, maxLength = 2)
-  val endHourField = getNewTimeField("H", 0, 23, maxLength = 2)
-  val endMinuteField = getNewTimeField("MM", 0, 59, maxLength = 2)
+  val eventLocationField = addValidationListeners(
+    p(placeholder := "Event location", contenteditable := "true").render,
+    validateForm)
+
+  val eventUrlField = addValidationListeners(p(
+    placeholder := "Event url",
+    contenteditable := "true",
+    `class` := "octopus-event-url"
+  ).render, validateForm)
+
+  /* used with time fields that don't have their own FieldWithError instance.
+   they call endMinuteField's instead */
+  def validateFormAndCheckTimeErrors(): Unit = {
+    endMinuteWrapper.makeDirty()
+    validateForm()
+  }
+
+  //time fields
+  val startHourField = getNewTimeField("H", 0, 23, maxLength = 2, validateFormAndCheckTimeErrors)
+  val startMinuteField = getNewTimeField("MM", 0, 59, maxLength = 2, validateFormAndCheckTimeErrors)
+  val endHourField = getNewTimeField("H", 0, 23, maxLength = 2, validateFormAndCheckTimeErrors)
+  val endMinuteField = getNewTimeField("MM", 0, 59, maxLength = 2, validateForm)
   val timezoneHourField = span("H").render
   val timezoneMinuteField = span("MM").render
 
@@ -45,39 +59,41 @@ class FormHandler(startDay: Date, octopusHome: Div) {
   timezoneHourField.textContent = sign + "%02d".format((tz / 60).abs)
   timezoneMinuteField.textContent = "%02d".format((-tz % 60).abs)
 
-  val eventLocationField = p(placeholder := "Event location", contenteditable := "true").render
-
-  val eventUrlField = p(
-    placeholder := "Event url",
-    contenteditable := "true",
-    `class` := "octopus-event-url"
-  ).render
-
   val messagesWithFields: Map[String, HTMLElement] = Map(
     MalformedDatesMessage -> endMinuteField,
     InvalidNameMessage -> eventNameField,
     InvalidDatesMessage -> endMinuteField,
-    InvalidOffsetMessage -> timezoneMinuteField,
+    //    InvalidOffsetMessage -> timezoneMinuteField,
     InvalidLocationMessage -> eventLocationField,
     InvalidURLMessage -> eventUrlField
   )
 
+  //validated fields wrappers
+  val eventNameWrapper = FieldWithErrorMessages(eventNameField, messagesWithFields)
+  val eventLocationWrapper = FieldWithErrorMessages(eventLocationField, messagesWithFields)
+  val eventUrlWrapper = FieldWithErrorMessages(eventUrlField, messagesWithFields)
+  val endMinuteWrapper = FieldWithErrorMessages(endMinuteField, messagesWithFields)
+
+  //and a list of them all
+  val fieldsWithValidation = List(eventNameWrapper, eventLocationWrapper, eventUrlWrapper, endMinuteWrapper)
+
+  //field for displaying results of sending the event to the server
   val messageField = p(`class` := "octopus-message hidden").render
 
   val submitButton: Button = button("Submit", `class` := "octopus-event-submit-button", onclick := submit _).render
 
   def view: List[HTMLElement] = List(
-    eventNameField,
+    eventNameWrapper.contents,
     p(DateOps.dateToString(startDay), `class` := "octopus-event-date").render,
     p(
       "From ",
       startHourField, ":",
       startMinuteField, " to ",
-      endHourField, ":", endMinuteField,
+      endHourField, ":", endMinuteWrapper.contents,
       " Timezone: UTC ", timezoneHourField, ":", timezoneMinuteField
     ).render,
-    eventLocationField,
-    eventUrlField,
+    eventLocationWrapper.contents,
+    eventUrlWrapper.contents,
     messageField,
     submitButton
   )
@@ -90,7 +106,7 @@ class FormHandler(startDay: Date, octopusHome: Div) {
     (startDay + (h hours) + (m minutes)).valueOf.toLong
   }
 
-  def buildEventOrGetErrors: Either[Event, Map[String, HTMLElement]] = {
+  def buildEventOrGetErrors: Either[Event, Set[String]] = {
     val millis = Try {
       getMillisFrom(startHourField, startMinuteField) -> getMillisFrom(endHourField, endMinuteField)
     }
@@ -110,16 +126,18 @@ class FormHandler(startDay: Date, octopusHome: Div) {
       url = eventUrlField.textContent,
       location = eventLocationField.textContent
     )
-    val timeFormatResult: Map[String, Boolean] = if (millis.isSuccess) Map.empty
-    else Map(MalformedDatesMessage -> false)
 
-    val messagesToShow = (invalidFieldsIn(baseEvent) ++ timeFormatResult)
-      .map { case (message, _) => message -> messagesWithFields(message) }
+    //optional error caused by bad time format (e.g. empty time fields)
+    val timeFormatResult: Set[String] =
+      if (millis.isSuccess) Set.empty
+      else Set(MalformedDatesMessage)
 
+    val messagesToShow = invalidFieldsIn(baseEvent) ++ timeFormatResult
     if (messagesToShow.isEmpty) Left(from(baseEvent))
     else Right(messagesToShow)
   }
 
+  //sends valid event to the server
   def sendEvent(event: Event) = {
     showMessage("Submitting event, please wait...")
 
@@ -143,34 +161,63 @@ class FormHandler(startDay: Date, octopusHome: Div) {
     }
   }
 
-  def submit(me: MouseEvent) =
+  def submit(me: MouseEvent) = {
     buildEventOrGetErrors match {
       case Left(event) => sendEvent(from(event))
-      case Right(errors) => /*todo show errors*/ errors foreach println
+      case Right(errors) =>
+        fieldsWithValidation.foreach(_.makeDirty())
+        showErrors(errors)
     }
+  }
 
   def showMessage(text: String) = {
     messageField.textContent = text
     show(messageField)
   }
 
-  def show(element: HTMLElement) = element.classList.remove("hidden")
+  def validateForm() =
+    buildEventOrGetErrors match {
+      case Right(errors) => showErrors(errors)
+      case _ => showErrors(Set.empty)
+    }
 
-  def hide(element: HTMLElement) = element.classList.add("hidden")
+  def showErrors(errors: Set[String]): Unit =
+    fieldsWithValidation.foreach(_.updateErrors(errors))
+
+  def show(element: HTMLElement) = element.classList.remove("hidden")
 }
 
 object FormHandler {
-  def getNewTimeField(hint: String, min: Int, max: Int, maxLength: Int) = {
-    val field = span(
-      placeholder := hint,
-      contenteditable := "true"
-    ).render
-    field.onkeypress = new IntOnlyKeyHandler(maxLength, min, max, field).handleEvent _
+  val MalformedDatesMessage = "We need to know when the event starts and ends!"
+
+  def getNewTimeField(hint: String, min: Int, max: Int, maxLength: Int,
+                      validationCallback: () => Unit): Span = {
+    val field = span(placeholder := hint, contenteditable := "true").render
+    val handler = new IntOnlyKeyHandler(maxLength, min, max, field, validationCallback)
+    field.onkeypress = (event: KeyboardEvent) => {
+      handler.handleEvent(event)
+    }
+    field.onblur = (event: FocusEvent) => {
+      validationCallback()
+    }
+    field
+  }
+
+  //adds keypress / focusleave listeners that call specified callback
+  def addValidationListeners[T <: HTMLElement](field: T, callback: () => Unit): T = {
+    val listener = (_: UIEvent) => {
+      field.parentElement.classList add "dirty"
+      timers.setTimeout(KeyCheckDelay)(callback())
+    }
+
+    field.onkeypress = listener
+    field.onblur = listener
+
     field
   }
 }
 
-class IntOnlyKeyHandler(maxLength: Int, min: Int, max: Int, view: HTMLElement) {
+class IntOnlyKeyHandler(maxLength: Int, min: Int, max: Int, view: HTMLElement, validationCallback: () => Unit) {
   var lastValidValue = view.textContent
 
   def handleValidKey(ch: Char): Unit =
@@ -179,15 +226,44 @@ class IntOnlyKeyHandler(maxLength: Int, min: Int, max: Int, view: HTMLElement) {
         view.textContent = lastValidValue
       else
         lastValidValue = view.textContent
+      validationCallback()
     }
 
-  def handleEvent(event: DomEvent) = event match {
-    case e: KeyboardEvent =>
-      val ch = e.charCode.toChar
-      Try(ch.toString.toInt) match {
-        case Success(_) => handleValidKey(ch)
-        // case Failure(_) if List('-', '+') contains ch => handleValidKey(ch)
-        case _ => event.preventDefault()
-      }
+  def handleEvent(e: KeyboardEvent) = {
+    val ch = e.charCode.toChar
+    Try(ch.toString.toInt) match {
+      case Success(_) => handleValidKey(ch)
+      // case Failure(_) if List('-', '+') contains ch => handleValidKey(ch)
+      case _ =>
+        //called here because the first case runs a delayed operation
+        validationCallback()
+        e.preventDefault()
+    }
   }
+}
+
+case class FieldWithErrorMessages[T <: HTMLElement] private(field: T, messageSource: Map[String, HTMLElement]) {
+  val messages = messageSource.filter(_._2 == field).keySet
+
+  def updateErrors(errors: Set[String]) = {
+    messages.zipWithIndex.partition(errors contains _._1) match {
+      case (invalid, valid) =>
+        invalid.foreach { case (_, index) => contents.children(1 + index).classList add "invalid" }
+        valid foreach { case (_, index) => contents.children(1 + index).classList remove "invalid" }
+    }
+  }
+
+  def getText = field.textContent
+
+  def makeDirty() = contents.classList add "dirty"
+
+  private val wrapperClass = Option(field.className).filterNot(_ == "").map(" " + _ + "-wrapper")
+
+  val contents = div(
+    `class` := "octopus-validation-parent" + wrapperClass.getOrElse(""),
+    field,
+    messages.map { message =>
+      span(`class` := "octopus-validation-message", "!", title := message).render
+    }.toList
+  ).render
 }
