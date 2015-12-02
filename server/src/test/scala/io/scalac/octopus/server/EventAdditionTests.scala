d9ff5c5ba287ccbdc03e253c56f47fb6f2a8ed01
Jakub Kozłowski <kubukoz@gmail.com>
2015-12-02 20:00:50 +0100

    Implement constraint server-side

--- server/src/test/scala/io/scalac/octopus/server/EventAdditionTests.scala
+++ server/src/test/scala/io/scalac/octopus/server/EventAdditionTests.scala
@@ -7,14 +7,19 @@ import scalac.octopusonwire.shared.domain._
 
 class EventAdditionTests extends OctoSpec {
   "A user" must "not be able to add an event if they aren't logged in" in {
-    unauthorizedApi addEvent sampleValidEvent shouldBe FailedToAdd(`User not logged in`)
+    unauthorizedApi addEvent sampleValidEvent shouldBe FailedToAdd(UserNotLoggedIn)
   }
 
   they must "not be able to add an event that ends in the past" in {
-    authorizedApi addEvent oldEvent shouldBe FailedToAdd(`The event can't end in the past`)
+    authorizedApi addEvent oldEvent shouldBe FailedToAdd(EventCantEndInThePast)
   }
 
-  they should "be able to add an event when logged in" in {
-    authorizedApi addEvent sampleValidEvent shouldBe Added()
+  they should "not be able to add an event not having X past items joined" in{
+    authorizedApi addEvent sampleValidEvent shouldBe FailedToAdd(UserCantAddEventsYet)
   }
+
+  they should "be able to add an event when logged in having X past events joined" in {
+    authorizedApiWithJoinedPastEvents addEvent sampleValidEvent shouldBe Added()
+  }
+
 }
